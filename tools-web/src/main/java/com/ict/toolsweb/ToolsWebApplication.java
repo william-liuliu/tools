4bde3dad8fe82cf77e01620353636d5e7e95e996
刘巍 <liuwei@liuweideMacBook-Pro.local>
2018-07-22 11:23:51 +0800

    Initial commit

---
+++ tools-web/src/main/java/com/ict/toolsweb/ToolsWebApplication.java
@@ -0,0 +1,12 @@
+package com.ict.toolsweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolsWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToolsWebApplication.class, args);
+    }
+}
